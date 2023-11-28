8af336f87529d5ba41e6426b200ff461df37f6f1
Hugo Serinese (RIT Student) <hhs2791@rit.edu>
2023-11-28 15:19:31 -0500

    trying callback functions

--- animationTest/option.js
+++ animationTest/option.js
@@ -32,7 +32,7 @@ p{
 
 <div>
 <p></p>
-<img src="animationTest/choice.png" alt="choose">
+<img src="animationTest/choice.png" alt="choose" id="button">
 </div>
 
 `;
@@ -46,27 +46,45 @@ class ChoiceButton extends HTMLElement{
         this.shadowRoot.appendChild(template.content.cloneNode(true));
 
         this.p = this.shadowRoot.querySelector("p");
+        // this.callbackFunction = null;
     }
 
     connectedCallback()
     {
         this.render();
+        // this.shadowRoot.querySelector('div').addEventListener('click',this.handler);
+    }
+
+    handler() {
+        console.log("this is the test");
+        console.log('test action');
     }
 
     attributeChangedCallback(attributeName, oldVal, newVal){
-        console.log(attributeName, oldVal, newVal);
+        // console.log(attributeName, oldVal, newVal);
         this.render();
     }
 
     static get observedAttributes(){
-        return ["text"];
+        return ["text","callback"];
     }
 
     render(){
         const text = this.getAttribute('text') ? this.getAttribute('text') : "null";
-
+        const callback = this.getAttribute('callback') ? this.getAttribute('callback') : null;
+        
         this.p.innerHTML = text;
+        // this.callbackFunction = callback;
+        // addFunction(this, callback);
     }
 }
 
 customElements.define('option-button', ChoiceButton);
+
+function addFunction(elem, callback)
+{
+    const shadow = elem.shadowRoot;
+    const childNodes = Array.from(shadow.childNodes);
+    // shadow.querySelector('div').addEventListener('click', callback);
+    console.log(callback);
+}
